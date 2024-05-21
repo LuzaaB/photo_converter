f9451492726c4e76e6b0db615aed3e17707496c7
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-05-21 11:51:21 +0545

    remove os, use pathlib

--- heic/heic_jpg.py
+++ heic/heic_jpg.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from pillow_heif import register_heif_opener
 from pathlib import Path
-import os
+# import os
 
 register_heif_opener()
 
@@ -11,16 +11,11 @@ print(parent_path)
 heic_path = parent_path / "heic_images"
 image_path = parent_path / "jpg_images"
 
-dir_list = os.listdir(heic_path)
-print("Directory List : ", dir_list)
+# dir_list = os.listdir(heic_path)
+# print("Directory List : ", dir_list)
 
-for each in dir_list:
-    if each.endswith(".heic") or each.endswith(".HEIC"):
-        img = Image.open(heic_path/each)
-        img.convert('RGB').save(f"{image_path}/{each[:-5]}.jpg")
-
-# # Open HEIF or HEIC file
-# image = Image.open('example.heic')
-
-# # Convert to JPEG
-# image.convert('RGB').save('example.jpg')
+for each in heic_path.iterdir():
+    file_name = Path(each).name
+    if file_name.endswith(".heic") or file_name.endswith(".HEIC"):
+        img = Image.open(each)
+        img.convert('RGB').save(f"{image_path}/{file_name[:-5]}.jpg")
