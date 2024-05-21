afac4792990c8808a323213dcac5b09e4c441db3
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-05-21 11:54:03 +0545

    remove os, use pathlib only

--- heic/heic_png.py
+++ heic/heic_png.py
@@ -1,6 +1,4 @@
 from heic2png import HEIC2PNG
-import os
-# import glob
 from pathlib import Path
 
 parent_path = Path(__file__).parent.parent.resolve()
@@ -9,26 +7,11 @@ print(parent_path)
 heic_path = parent_path / "heic_images"
 image_path = parent_path / "png_images"
 
-dir_list = os.listdir(heic_path)
-# print("Files and directories in '", path, "' :")
-# print(dir_list)
-for each in dir_list: # os.listdir() can also be used
-    print("each ", each)
-    if each.endswith(".heic") or each.endswith(".HEIC"):
-        print(each)
-        img = HEIC2PNG(heic_path / each, quality=70)
-        img.save(f"{image_path}/{each[:-4]}.png")
-
-
-
-
-# files = [f for f in pathlib.Path().iterdir() if f.is_file()]
-# print(files)
-
-# file = [f for f in pathlib.Path().glob("/photo_converter/*.png")]
-# print(file)
-
-# print(glob.glob("/photo_converter/*.png"))
-
-# files = [f for f in os.listdir() if os.path.isdir(f)]
-# print(files)
+# dir_list = os.listdir(heic_path)
+# # print("Files and directories in '", path, "' :")
+# # print(dir_list)
+for each in heic_path.iterdir():
+    file_name = Path(each).name
+    if file_name.endswith(".heic") or file_name.endswith(".HEIC"):
+        img = HEIC2PNG(each, quality=70)
+        img.save(f"{image_path}/{file_name[:-4]}.png")
