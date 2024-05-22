9c3ff05fec09d012a7989b880446af334b76f1ab
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-05-22 12:02:15 +0545

    complete heic to pdf (takes spaace)

--- heic/heic_pdf.py
+++ heic/heic_pdf.py
@@ -1,6 +1,6 @@
 import img2pdf
 from pathlib import Path
-import os
+# from heic2png import HEIC2PNG
 
 parent_path = Path(__file__).parent.parent.resolve()
 print(parent_path)
@@ -10,11 +10,10 @@ print(heic_path)
 pdf_path = parent_path / "pdf_images"
 print(pdf_path)
 
-dir_list = os.listdir(heic_path)
-print(dir_list)
-
-for each in dir_list:
-    print("each",each)
-    if each.endswith("heic") or each.endswith(".HEIC"):
-        with open(heic_path, "wb") as f:
+for each in heic_path.iterdir():
+    file_name = Path(each).name
+    if file_name.endswith(".heic") or file_name.endswith(".HEIC"):
+        # img = HEIC2PNG(each, quality=70)
+        pdf_name = pdf_path / f"{file_name[:-5]}.pdf"
+        with open(pdf_name, "wb") as f:
                 f.write(img2pdf.convert(each))
