2cf2316a590a1bdce96632bf20f4a47ed7baf4a4
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-05-24 13:24:45 +0545

    complete png to jpg

---
+++ png/png_jpg.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from pathlib import Path
+
+parent_path = Path(__file__).parent.parent.resolve()
+
+png_path = parent_path / "png_images"
+print(png_path)
+jpg_path = parent_path / "jpg_images"
+print(jpg_path)
+
+for each in png_path.iterdir():
+    file_name = Path(each).name
+    print(each)
+    img = Image.open(each)
+    image = img.convert('RGB').save(f"{jpg_path}/{file_name[:-4]}.jpg")    
