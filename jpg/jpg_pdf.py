773a8763d4e9e7f63fcae06addca974c1ee8e571
Luzaa Byanjankar <luzaabyanjankar123@gmail.com>
2024-05-24 16:49:37 +0545

    add endswith

--- jpg/jpg_pdf.py
+++ jpg/jpg_pdf.py
@@ -8,8 +8,8 @@ pdf_path = parent_path / "pdf_images"
 
 for each in jpg_path.iterdir():
     file_name = Path(each).name
-    # pdf_name = file_name[:-4]
-    pdf_name = pdf_path / f"{file_name[:-4]}.pdf"
-    with open(pdf_name, "wb") as f :
-        f.write(img2pdf.convert(each))
+    if file_name.endswith(".JPG") or file_name.endswith(".jpg"):
+        pdf_name = pdf_path / f"{file_name[:-4]}.pdf"
+        with open(pdf_name, "wb") as f :
+            f.write(img2pdf.convert(each))
         
